8d84ccb74fd55aff27cc364d65ffd200ccd13de0
Jin-devST <kpasd002@gmail.com>
2023-12-10 18:15:49 +0900

    set project constructor

---
+++ src/module/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { AppController } from "../controller";
+import { AppService } from "../service";
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor() {
+  }
+}
